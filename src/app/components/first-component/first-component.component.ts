579d688c86db0f00c859c3e8c517fb5c29476b1c
Patrick Velasques <pettyvelasques@hotmail.com>
2022-09-24 20:48:38 -0300

    Interpolação de Dados

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Patrick';
+  age: number = 29;
+  job = 'Mecânico';
+  hobbies = ['Jogar futebol', 'Estudar', 'Viajar de moto'];
+  car = {
+    name: 'Chevette',
+    year: 1991,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
